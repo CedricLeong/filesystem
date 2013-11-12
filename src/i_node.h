8cd6f35176108ed0244523209cad65a7705fda4f
cedric leong <cedric.leong@uoit.net>
2013-11-12 15:54:02 -0500

    Update i_node.h

--- src/i_node.h
+++ src/i_node.h
@@ -12,6 +12,8 @@ extern int put_inode_table();
 extern int get_file_pointer(int i_number,int* file_ptr);
 extern int alloc_block_tofile(int i_number, int *allocated_blkno);
 extern int parse_pathname(char *path,int i_number);
+extern int put_file(int i_number, int file_ptr, int type);
+extern int get_file(int i_number, int *type);
 
 
 #endif
