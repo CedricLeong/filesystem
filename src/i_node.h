757d9bdb601e6613d693555fd5f17359429977fe
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2013-11-12 15:55:29 -0500

    Added struct inode to i_node.h

--- src/i_node.h
+++ src/i_node.h
@@ -1,6 +1,8 @@
 #ifndef I_NODE_H_   /* Include guard */
 #define I_NODE_H_
 
+#define MAX_NAME_LENGTH 5
+
 extern short int file_blockno[8][64];
 extern short int file_pointer[64]; 
 extern short int file_refcount[64];
@@ -14,6 +16,15 @@ extern int alloc_block_tofile(int i_number, int *allocated_blkno);
 extern int parse_pathname(char *path,int i_number);
 extern int put_file(int i_number, int file_ptr, int type);
 extern int get_file(int i_number, int *type);
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    char *time_created;
+    char *time_last_accessed;
+    char *time_last_modified;
+    int file_size;
+    int index_blk_location;
+} inode;
 
 
 #endif
