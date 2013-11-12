6a78026718743d77d6f204d682515431a5181913
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2013-11-12 18:56:21 -0500

    Added sfs_create.h file.

---
+++ src/sfs_create.h
@@ -0,0 +1,23 @@
+#ifndef SFS_CREATE_H_   /* Include guard */
+#define SFS_CREATE_H_
+
+
+/***************************************************************************
+ *
+ * @brief File function to create files
+ * @details This function will create a new file along with its inode
+ *
+ * @author Yasha Prikhodko
+ * @author Cedric Leong
+ * @author Arezou Mousavi
+ *
+ * @param *pathname The absolute pathname of the file to be created
+ * @param type The type of the file to be created (0=regular file, 1=folder)
+ *
+ * @copyright GNU Public License.
+ *
+ ***************************************************************************/
+int sfs_create(char *pathname, int type)
+
+
+#endif
