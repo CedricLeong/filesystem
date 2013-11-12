9964894424d4e48a630b24689afe92ef7821479a
cedric leong <cedric.leong@uoit.net>
2013-11-12 18:19:34 -0500

    Update file_functions.c
    somewhat functional sfs_readfile, and sfs_close

--- src/file_functions.c
+++ src/file_functions.c
@@ -1,13 +1,14 @@
 #include "inode.h"
 #include "file_functions.h"
 
+short int next = 0; 
 
 int sfs_create(char *pathname, int type)
 {
 	if (type == 0)
 	{
 		//Create new inode
-		inode* inode = {free_block.get_free_block,date(NULL),0,free_block.get_free_block, pathname}; 
+
 		// put it in inode table
 		put_inodetable = inode; 
 		// create new index block
@@ -19,34 +20,67 @@ int sfs_open(char *pathname)
 	// Check if file is there
 	// Open the file
 	// return fd
+    if(pathname = strtok(pathname, DELIMS)) {
+        printf(pathname);
+    }
+    int *index;
+    int *i_numb;
+	if (compare_component_tobuff(pathname, index, i_numb) == 0) {
+        int *file_ptr;
+        get_file_pointer(i_numb, file_ptr);
+        refcount++;
+	} else {
+        return 1;
+	}
 }
 
-int sfs_read(int fd,int start, int length, char *mem_pointer)
+int sfs_read(int fd,int start, int length, char* buffer)
 {
-	//Copy contents of file into buffer
-	// read buffer
+	char* buffer;
+	buffer = calloc(1024, sizeof(char));
+
+ 	if (file_refcount[fd_table[fd]]== 4)
+ 		return 0;
+ 	else 
+ 		return 1; 
+
+
+ 	int success = get_file(fd_table[fd],0); 
+ 		if (success == 0)
+ 			return 0; 
+ 	return 1; 
 
 }
 
 int sfs_write(int fd, int start, int length, char *mem_pointer)
 {
-	//Copy buffer into file
-	// put block
+
 }
 
 int sfs_readdir(int fd, char *mem_pointer)
 {
-	// list all inodes
-
+	char buffer[6];
+	for (int i = 0; i < 6; i++)
+	{
+		buffer[i] = pathname[i][next];
+	}
+	mem_pointer = buffer;
+	next++; 
+	return 1; 
 }
 
 int sfs_close(int fd)
 {
-	// Close fd in swoft 
+	if ( file_refcount[file_table[fd]] == 0)
+		return 0; // file not open
+	else
+		 file_refcount[file_table[fd]]--;
 
+  return 1; 
 }
 
 int sfs_delete(char *pathname)
 {
-	// wipe file
+	// deletle pathname
+	// deletle inumber
 }
