a500193a620fe421cb15c07fb081c92591a6dd43
cedric leong <cedric.leong@uoit.net>
2013-11-11 19:40:14 -0500

    Update i_node.c

--- src/i_node.c
+++ src/i_node.c
@@ -1,25 +1,40 @@
 #include i_node.h
 #include blockio.h
+#include <math.h>
 
+// Global data that should be visible to inode and super block
 short int file_blockno[8][64];
 short int file_pointer[64]; 
 short int file_refcount[64];
 short int fd_table[64];
+char pathname_parse[6][64];
 
+/* Creates the index numbers in memory then in the disk itself
+Each file gets around 16 bytes so each block can hold 8 inodes*/
 int put_inode_table():
 {
+	for (int a = 1; a < 65; a++)
+	{
+		file_blockno[0][a-1] = a;
+	}
+	int inum =1; 
 	int blockno = 0;
 	int j = 2;
     // Block Number
 	while (j < 10)
 	{
 		int *buf;
-    	int = calloc(64, sizeof(int));
+    	buf = calloc(64, sizeof(int));
 		// Byte numbers
-        for(int i=0; i < 64; i++) 
+        for(int i=1; i < 129; i++) 
         {
-        	buf[i] = blockno;
-        	blockno++;
+        	if ((i % 8) == 0)
+        	{
+        		buf[i-1]= inum;
+        		inum++;
+        	}
+        	else
+        		buf[i-1]= 0;
 
         }
         int writeintable = put_block(j, buf);
@@ -27,29 +42,34 @@ int put_inode_table():
     return 1;
 }
 
+/* Retrieves the file pointer from memory, it will be changed to disk soon*/
 int get_file_pointer(int i_number,int* file_ptr)
 {
-	file_ptr = blockno[2][i_number];
+	file_ptr = file_blockno[2][i_number];
 	return 1; 
 }
 
-int parse_dir_entry(int component_no, char 
-*component, int *i_number)
+/* Allocates the block by retrieving the file pointer from memory
+Then looking at the value of the pointer it sees how many blocks 
+the file has. If it can it will assign a new block to the file*/
+int alloc_block_tofile(int i_number, int *allocated_blkno)
 {
-
-}
-
-int alloc_block_tofile(int i_number, int 
-*allocated_blkno)
-{
-	int *buf;
-    buf = calloc(128, sizeof(int));
-    int load = get_block( i_number +2, buf);
     int numblks = get_file_pointer(i_number);
-    if (numblks > 8)
+    if (&numblks > 8)
     	return 0;
     allocated_blockno = get_super_blk();
-    int load = get_block( 10, buf);
-    // add 1 to the entry in block 10; 
+    file_blockno[1][i_number-1]++; 
     return 1; 
 }
+
+/* Saves the path name in an array so it can be used for comparison later.*/ 
+
+int parse_pathname(char *path,int i_number)
+{
+	for (int i =0; i < 5; i++)
+	{
+		pathname_parse[i][i_number] = path[i];
+	}
+	pathname_parse[5] = '\0';
+
+} 
