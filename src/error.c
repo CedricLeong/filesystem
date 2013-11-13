1ec040f490e9bd34d121f952524cd36ede8b2429
cedric leong <cedric.leong@uoit.net>
2013-11-13 00:31:56 -0500

    Update error.c

--- src/error.c
+++ src/error.c
@@ -8,5 +8,20 @@ int error(ERROR error) {
         case NO_FILE_NAME_ENTERED:
             printf("%s\n", "No name for the file was provided\nReturn value: -1");
             return -1;
+        case FULL_FILE:
+        	printf("%s\n", "The file has the maximum 8 block allocation and is full.\nReturn value: -1");
+        	return -1;
+        case PUT_BLOCK_FAIL:
+        	printf("%s\n", "Error occurred when writing from memory to disk block.\nReturn value: -1");
+        	return -1;
+        case GET_BLOCK_FAIL:
+        	printf("%s\n", "Error occurred when reading from disk to memory.\nReturn value: -1");
+        	return -1;
+        case BLK_ALLOCATED:
+        	printf("%s\n", "A block has been successfully allocated to the file.\nReturn value: 0");
+        	return -1;
+        case FAIL_ALLOCATE:
+        	printf("%s\n", "A block has been unsuccessfully allocated to the file.\nReturn value: -1");
+        	return -1;
     }
 }
